d388f54707b7b8a145ad1c4b8c561fdd0e111c3b
Adrian Ardizza <tmgfaction@gmail.com>
2023-03-11 23:23:06 +0700

    Implement Token authentication

--- myapp/settings.py
+++ myapp/settings.py
@@ -42,8 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_yasg',
     'scheduler',
+    'authentication',
 ]
 
 MIDDLEWARE = [
